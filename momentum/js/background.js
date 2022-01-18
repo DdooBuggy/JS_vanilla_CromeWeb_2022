3a643e9674bbf826818cafe8d9d85ec1b75efd82
DdooBuggy <yhiyo03@gmail.com>
2022-01-18 15:10:17 +0900

    Random Background(~#6.2)

---
+++ momentum/js/background.js
@@ -0,0 +1,15 @@
+const images = [
+    "0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg",
+];
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
+const imageNumber = getRandomInt(0, images.length);
+const chosenImage = images[imageNumber];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+document.body.appendChild(bgImage);
