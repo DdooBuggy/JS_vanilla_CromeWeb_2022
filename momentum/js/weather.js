7c17fb040c578d8a28fdf5baace1f96b8b77c3c2
DdooBuggy <yhiyo03@gmail.com>
2022-01-19 14:49:38 +0900

    Weather and Geolocation(~#8.2)

---
+++ momentum/js/weather.js
@@ -0,0 +1,16 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "f047a860a4ee2cdc7ecf19fd0e744d7d";
+
+function onGeoOK(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {city.innerText = data.name; weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;});
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
