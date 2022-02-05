40cd7346d3fa9ad3c0d04142885ea2b77f551935
DdooBuggy <yhiyo03@gmail.com>
2022-02-05 11:07:59 +0900

    mediaQuery, loginForm, clock

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -1,16 +1,25 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const todoBox = document.querySelector(".todo-box");
+const clockBox = document.querySelector(".clock-box")
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+const CLOCKPOSITION_CLASSNAME = "clock-initial-position";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    loginInput.classList.add(HIDDEN_CLASSNAME);
+    clockBox.classList.remove(CLOCKPOSITION_CLASSNAME);
     const typedUsername = loginInput.value;
     localStorage.setItem(USERNAME_KEY, typedUsername);
     paintGreetings(typedUsername)
+}
+
+function paintTotoBox() {
+    todoBox.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function paintGreetings(username) {
@@ -20,7 +29,13 @@ function paintGreetings(username) {
 
 if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+    
 } else {
     paintGreetings(savedUsername);
+    paintTotoBox();
+    // loginForm id add or remove?
+    loginInput.classList.add(HIDDEN_CLASSNAME);
+    clockBox.classList.remove(CLOCKPOSITION_CLASSNAME);
 }
+loginForm.addEventListener("submit", onLoginSubmit);
+loginForm.addEventListener("submit", paintTotoBox);
