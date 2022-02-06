58b4b7617359f839c416ddc33c8242c00a891b61
DdooBuggy <yhiyo03@gmail.com>
2022-02-06 18:10:52 +0900

    todos

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -1,7 +1,6 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector("#greeting");
-const todoBox = document.querySelector(".todo-box");
 const clockBox = document.querySelector(".clock-box")
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -18,10 +17,6 @@ function onLoginSubmit(event) {
     paintGreetings(typedUsername)
 }
 
-function paintTotoBox() {
-    todoBox.classList.remove(HIDDEN_CLASSNAME);
-}
-
 function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
@@ -32,10 +27,8 @@ if (savedUsername === null) {
     
 } else {
     paintGreetings(savedUsername);
-    paintTotoBox();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     loginInput.classList.add(HIDDEN_CLASSNAME);
     clockBox.classList.remove(CLOCKPOSITION_CLASSNAME);
 }
 loginForm.addEventListener("submit", onLoginSubmit);
-loginForm.addEventListener("submit", paintTotoBox);
--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -1,7 +1,9 @@
-const toDoForm = document.getElementById("todo-form");
+const todoWindow = document.querySelector(".todo-window");
 const toDoList = document.getElementById("todo-list");
-const toDoInput = toDoForm.querySelector("input");
+const toDoInput = todoWindow.querySelector("input");
+const todoBtn = document.querySelector(".todo-btn")
 const TODOS_KEY = "toDos";
+const TODO_CHECKED = "todo-checked";
 
 let toDos = [];
 function saveToDos() {
@@ -9,25 +11,59 @@ function saveToDos() {
 }
 
 function deleteToDo(event) {
-    const li = event.target.parentElement.parentElement;
+    const li = event.target.parentElement;
     li.remove();
     toDos = toDos.filter((item) => item.id !== parseInt(li.id));
     saveToDos();
 }
 
+function checkToDo(event) {
+    const checkedIcon = document.createElement("i");
+    const icon = document.createElement("i");
+    const clickedBtn = event.target.parentElement;
+    const li = clickedBtn.parentElement.parentElement;
+
+    icon.setAttribute("class", "far fa-square");
+    checkedIcon.setAttribute("class", "far fa-check-square");
+    
+
+    if (li.classList.contains(TODO_CHECKED)) {
+        event.target.remove();
+        clickedBtn.appendChild(icon);
+        icon.addEventListener("click", checkToDo);
+    } else {
+        event.target.remove();
+        clickedBtn.appendChild(checkedIcon);
+        checkedIcon.addEventListener("click", checkToDo);
+    }
+
+    li.classList.toggle(TODO_CHECKED);
+}
+
 function paintToDo(toDoText) {
     const li = document.createElement("li");
+    const div = document.createElement("div");
+    const button = document.createElement("button");
+    const span = document.createElement("span");
+    const icon = document.createElement("i");
+    
+    const trashIcon = document.createElement("i");
+    icon.setAttribute("class", "far fa-square");
+   
+    trashIcon.setAttribute("class", "fas fa-trash");
+ 
     li.id = toDoText.id;
     li.classList.add("todo-list__component");
-    const span = document.createElement("span");
     span.innerText = toDoText.text;
-    const button = document.createElement("button");
-    const icon = document.createElement("i");
-    icon.setAttribute("class", "far fa-square");
+    
+    div.appendChild(button);
+    div.appendChild(span);
     button.appendChild(icon);
-    button.addEventListener("click", deleteToDo);
-    li.appendChild(span);
-    li.appendChild(button);
+    icon.addEventListener("click", checkToDo);
+    trashIcon.addEventListener("click", deleteToDo);
+
+    li.appendChild(div);
+    li.appendChild(trashIcon);
     toDoList.appendChild(li);
 }
 
@@ -38,12 +74,13 @@ function handleToDoSubmit(event) {
  const newToDoObj = {
      text: newToDo,
      id: Date.now(),
+     checked:0,
  };
  toDos.push(newToDoObj);
  paintToDo(newToDoObj);
  saveToDos();
 }
-toDoForm.addEventListener("submit", handleToDoSubmit);
+todoWindow.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
@@ -51,3 +88,10 @@ if (savedToDos !== null) {
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
 }
+
+function todoWindowToggle(event) {
+    event.preventDefault();
+    todoWindow.classList.toggle(HIDDEN_CLASSNAME);
+}
+
+todoBtn.addEventListener("click", todoWindowToggle)
